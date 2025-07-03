ce925e851e5afe138a86a2c7516b7802046b54af
manu <emanuelepires2000@gmail.com>
2025-07-03 11:55:04 -0300

    Inicializando projeto.

---
+++ src/test/java/in/manu/springbootcrud/springbootcrudapi/SpringbootcrudapiApplicationTests.java
@@ -0,0 +1,13 @@
+package in.manu.springbootcrud.springbootcrudapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootcrudapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
